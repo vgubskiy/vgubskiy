064c9199c8b06e4265d8fa6b1d8f96f6de602fd3
Valeriy Gubskiy <vgubskiy@gmail.com>
2017-06-23 12:54:30 +0300

    JavaDoc

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,19 @@
 package ru.job4j;
 
+/**
+ * Класс выводит в консоль строку Hello world!
+ * @author Valeriy Gubskiy
+ * @since 23.06.2017
+ * @version 1.0
+ */
+
 class Calculate {
+	
+	/**
+	 * Метод main является точкой входа для выполнения программы
+	 * @param args в качестве аргументов принимает массив строк 
+	 */
+	
 	public static void main (String[] args) {
 		System.out.println("Hello World!");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for Calculate task
+*
+* @author Valeriy Gubskiy
+* @version 1.0
+* @since 22.06.2017
+*/
+package ru.job4j;
