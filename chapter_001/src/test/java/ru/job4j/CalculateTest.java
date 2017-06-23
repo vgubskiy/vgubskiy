4e829aab04605cc8f1c3479afa47e82b9d342c86
Valeriy Gubskiy <vgubskiy@gmail.com>
2017-06-23 13:41:04 +0300

    JunitTest

---
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -0,0 +1,33 @@
+package ru.job4j;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test
+ *
+ * @author Valeriy Gubskiy
+ * @since 23.06.2017
+ * @version 1.0
+ */
+
+public class CalculateTest {
+	
+	/**
+	 * Test add
+	 */
+	@Test
+	public void whenAddOneToOneThenTwo() {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(out));
+		Calculate.main(null);
+		assertThat(
+			out.toString(),
+			is(String.format("Hello World!%s", System.getProperty("line.separator")))
+		);
+	}
+}
+ 
