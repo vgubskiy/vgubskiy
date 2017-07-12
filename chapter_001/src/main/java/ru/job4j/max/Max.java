a045480333ebae12b630857bff04d10a285dbc8a
Valeriy Gubskiy <vgubskiy@gmail.com>
2017-07-12 10:37:15 +0300

    Исправлен метод Max.max() в задании [#18978]

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -25,7 +25,6 @@ public class Max {
 	* @return - возвращет максимальное из трех
 	*/
 	public int max(int first, int second, int third) {
-		Max max = new Max();
-		return max.max(max.max(first, second), third);
+		return max(max(first, second), third);
 	}
 }
