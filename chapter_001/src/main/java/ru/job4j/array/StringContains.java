40896d7a4f01ea16a7cc7eed6bfd6908002e8d7a
Valeriy Gubskiy <vgubskiy@gmail.com>
2017-07-19 13:49:06 +0300

    Изменен алгоритм поиска слова в другом слове [#19661]

--- chapter_001/src/main/java/ru/job4j/array/StringContains.java
+++ chapter_001/src/main/java/ru/job4j/array/StringContains.java
@@ -7,7 +7,7 @@ package ru.job4j.array;
  * @since 18.07.2017
  */
 public class StringContains {
-	/**.
+    /**.
 	 * метод проверяет если ли в исходной строке заданная строка
 	 * @param orig - исходная строка
 	 * @param sub - поисковая строка
@@ -16,9 +16,17 @@ public class StringContains {
 	public boolean cont(String orig, String sub) {
         int lenOrig = orig.length();
         int lenSub = sub.length() - 1;
+        int count = 0;
         for (int i = 0; i < lenOrig; i++) {
             if (orig.charAt(i) == sub.charAt(0) && i + lenSub < lenOrig && orig.charAt(i + lenSub) == sub.charAt(lenSub)) {
-                if (orig.substring(i, i + lenSub + 1).equals(sub)) {
+                for (int j = 0; j <= lenSub; j++) {
+                    if (sub.charAt(j) == orig.charAt(i + j)) {
+                        count++;
+                    } else {
+                        break;
+                    }
+                }
+                if (count == lenSub + 1) {
                     return true;
                 }
             }
